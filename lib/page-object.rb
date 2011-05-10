cfb4b7a0a53f21d248e2f00fad7d02b2c8fec193
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-05-10 12:51:23 -0400

    added final case - passing a bad driver

--- lib/page-object.rb
+++ lib/page-object.rb
@@ -14,8 +14,10 @@ module PageObject
   def include_platform_module
     if @browser.kind_of? Watir::Browser
       self.class.send :include, PageObject::WatirPageObject
-    else
+    elsif @browser.kind_of? Selenium::WebDriver::Driver
       self.class.send :include, PageObject::SeleniumPageObject
+    else
+      raise ArgumentError, "expect Watir::Browser or Selenium::WebDriver::Driver"
     end
   end
 end
--- spec/page-object/page-object_spec.rb
+++ spec/page-object/page-object_spec.rb
@@ -6,34 +6,28 @@ end
 
 describe PageObject do
   context "when created with a watir-webdriver browser" do
-    before(:each) do
-      @browser = Watir::Browser.new :firefox
-    end
-    
-    after(:each) do
-      @browser.close
-    end
-    
     it "should include the WatirPageObject module" do
+      @browser = Watir::Browser.new :firefox
       page_object = TestPageObject.new(@browser)
       page_object.should be_kind_of PageObject::WatirPageObject
+      @browser.close
     end
   end
 
   context "when created with a selenium browser" do
-    before(:each) do
-      @browser = Selenium::WebDriver.for :firefox
-    end
-
-    after(:each) do
-      @browser.close
-    end
-    
     it "should include the SeleniumPageObject module" do
+      @browser = Selenium::WebDriver.for :firefox
       page_object = TestPageObject.new(@browser)
       page_object.should be_kind_of PageObject::SeleniumPageObject
+      @browser.close
     end
-
   end
-
+  
+  context "when created with an object we do not understand" do
+    it "should throw an error" do
+      expect {
+        TestPageObject.new("blah")
+      }.to raise_error
+    end
+  end
 end
