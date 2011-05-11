1ad0ac4f0e08c141a4582c1d8b6d4755ee443a4b
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-05-11 08:38:51 -0400

    added basic rdoc and a travis build status to the readme

--- lib/page-object.rb
+++ lib/page-object.rb
@@ -1,13 +1,17 @@
 
 require 'page-object/version'
 
+# Module that when included adds core functionality to a page object.
 module PageObject
   attr_reader :driver
-  
+
+  # Construct a new page object.  The browser parameter must be either a
+  # Watir::Browser or Selenium::WebDriver::Driver.
   def initialize(browser)
     include_platform_driver(browser)
   end
-
+  
+  # Gets the entire text of the page
   def text
     driver.text
   end
