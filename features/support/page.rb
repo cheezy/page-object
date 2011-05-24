66f5c906c5ea76f8c914cc08018a069c87c80a8f
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-05-24 13:07:46 -0400

    fixed url in link method call

--- features/support/page.rb
+++ features/support/page.rb
@@ -17,7 +17,7 @@ class Page
   link(:google_search_xpath, :xpath => "//a[text()='Google Search']")
   link(:google_search_link, :link => "Google Search")
   link(:google_search_link_text, :link_text => "Google Search")
-  link(:google_search_href, :href => "http://www.google.com")
+  link(:google_search_href, :href => "success.html")
   link(:google_search_text, :text => "Google Search")
   link(:google_search_index, :index => 0)
   
