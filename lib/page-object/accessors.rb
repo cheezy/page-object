57ced418eb6281ecf2b8a6f7f30d18c3987ca7df
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-08-01 06:43:36 -0400

    updated docs

--- lib/page-object/accessors.rb
+++ lib/page-object/accessors.rb
@@ -397,6 +397,7 @@ module PageObject
     #   * :id => Watir and Selenium
     #   * :index => Watir only
     #   * :name => Watir and Selenium
+    #   * :text => Watir and Selenium
     #   * :xpath => Watir and Selenium
     # @param optional block to be invoked when element method is called
     #
