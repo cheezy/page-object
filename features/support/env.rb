260de182e2da775880db3e73fecdd254fe8be279
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-12-27 14:47:58 -0500

    Moved before hook to hooks file

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,15 +3,5 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 require 'rspec/expectations'
 require 'watir-webdriver'
 require 'selenium-webdriver'
-
 require 'page-object'
-
-
-
-Before do
-  @browser = PageObject::PersistantBrowser.get_browser
-end
-at_exit do
-  PageObject::PersistantBrowser.quit
-end
 
---
+++ features/support/hooks.rb
@@ -0,0 +1,7 @@
+
+Before do
+  @browser = PageObject::PersistantBrowser.get_browser
+end
+at_exit do
+  PageObject::PersistantBrowser.quit
+end
