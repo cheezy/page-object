353fd85fa611a398385dbe84c4345303d2959e99
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-01-04 20:03:48 -0500

    removed comment and made element method public

--- lib/page-object/element_locators.rb
+++ lib/page-object/element_locators.rb
@@ -5,26 +5,15 @@ module PageObject
       ::PageObject::LocatorGenerator.generate_locators(cls)
     end
 
+    def element(tag, identifier={:index => 0})
+      platform.element_for(tag, identifier.clone)
+    end
+
     private
 
     def locator(identifier)
       identifier[0] ? identifier[0] : {:index => 0}
     end
 
-    #
-    # Finds an element
-    #
-    # @param [Symbol] the name of the tag for the element
-    # @param [Hash] identifier how we find an element.  You can use a multiple paramaters
-    #   by combining of any of the following except xpath.  The valid keys are:
-    #   * :class => Watir and Selenium
-    #   * :id => Watir and Selenium
-    #   * :index => Watir and Selenium
-    #   * :name => Watir and Selenium
-    #   * :xpath => Watir and Selenium
-    #
-    def element(tag, identifier={:index => 0})
-      platform.element_for(tag, identifier.clone)
-    end
   end
 end
