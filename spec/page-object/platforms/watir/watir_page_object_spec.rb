c860fb8062144182d5290e6f8736bb20ec998afe
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2016-12-21 20:45:26 -0500

    removed outdated specs

--- spec/page-object/platforms/watir/watir_page_object_spec.rb
+++ spec/page-object/platforms/watir/watir_page_object_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 describe PageObject::Platforms::WatirWebDriver do
-  describe "create page object" do
-    let(:browser) { double('browser') }
-    let(:subject) { PageObject::Platforms::WatirWebDriver.create_page_object(browser) }
-    
-    it "should create a WatirPageObject" do
-      expect(subject).to be_kind_of PageObject::Platforms::WatirWebDriver::PageObject
-    end
-    
-    it "should give the watir page object the browser" do
-      expect(subject.browser).to be browser
-    end
-  end
-  
   describe "is for?" do
     it "should be true when the browser is Watir::Browser" do
       browser = mock_watir_browser
